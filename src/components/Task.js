d1f9d6d1f982f172825d804e5d9c8ba5a2001374
Grace Adelaka <adelakagrace@gmail.com>
2022-03-11 09:18:18 +0000

    added functionality to delete btn and toggleReminder

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {FaTrash} from 'react-icons/fa';
+import { TaskContext } from './TaskContext';
 
 const Task = ({task}) => {
+    const [tasks, setTasks] = useContext(TaskContext);
+
+    const handleDelete = (id) => {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
+    const toggleReminder = (id) => {
+       setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
+    } 
   return (
-    <div className='task'>
-        <h3>{task.textToDo} <FaTrash/></h3>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onClick={() => toggleReminder(task.id)}>
+        <h3>{task.textToDo} <FaTrash onClick={() => handleDelete(task.id)} style={{color: 'darkRed'}}/></h3>
         <p>{task.day}</p>
 
     </div>
